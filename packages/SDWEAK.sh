3639778231ef3c80370de5a86661179bba3603e4
Jorge Luis Silvério de Lima <jorgezarpon@msn.com>
2026-02-14 12:52:30 -0300

    Update SDWEAK.sh

--- packages/SDWEAK.sh
+++ packages/SDWEAK.sh
@@ -36,8 +36,6 @@ write /proc/sys/kernel/perf_event_max_sample_rate 1
 write /proc/sys/kernel/perf_event_max_stack 1
 write /proc/sys/kernel/seccomp/actions_logged ""
 write /proc/sys/kernel/io_delay_type 3
-write /proc/sys/dev/hpet/max-user-freq 2048
-write /sys/class/rtc/rtc0/max_user_freq 2048
 write /proc/sys/kernel/timer_migration 0
 write /proc/sys/kernel/watchdog 0
 write /proc/sys/kernel/soft_watchdog 0
@@ -51,31 +49,27 @@ write /proc/sys/kernel/printk_devkmsg off
 write /proc/sys/debug/exception-trace 0
 
 # vm
-write /proc/sys/vm/dirty_background_bytes 209715200
-write /proc/sys/vm/dirty_bytes 419430400
-write /proc/sys/vm/dirty_expire_centisecs 1500
-write /proc/sys/vm/dirty_writeback_centisecs 1500
-write /proc/sys/vm/min_free_kbytes 121634
+write /proc/sys/vm/dirty_background_bytes 264857600
+write /proc/sys/vm/dirty_bytes 2147483648
+write /proc/sys/vm/dirty_expire_centisecs 4500
+write /proc/sys/vm/dirty_writeback_centisecs 2000
 write /proc/sys/vm/hugetlb_optimize_vmemmap 0
 write /proc/sys/vm/page-cluster 0
-write /proc/sys/vm/page_lock_unfairness 8
-write /proc/sys/vm/vfs_cache_pressure 66
+write /proc/sys/vm/page_lock_unfairness 1
+write /proc/sys/vm/vfs_cache_pressure 50
 write /proc/sys/vm/watermark_scale_factor 125
-write /proc/sys/vm/swappiness 40
+write /proc/sys/vm/swappiness 100
 write /proc/sys/vm/watermark_boost_factor 0
-write /proc/sys/vm/stat_interval 15
+#write /proc/sys/vm/stat_interval 5
 write /proc/sys/vm/compact_unevictable_allowed 0
 write /proc/sys/vm/compaction_proactiveness 10
 
 # mm
-write /sys/kernel/mm/transparent_hugepage/enabled always
-write /sys/kernel/mm/transparent_hugepage/khugepaged/defrag 1
+write /sys/kernel/mm/transparent_hugepage/enabled madvise
+write /sys/kernel/mm/transparent_hugepage/khugepaged/defrag 0
 write /sys/kernel/mm/transparent_hugepage/defrag defer+madvise
 write /sys/kernel/mm/ksm/run 0
 write /sys/kernel/mm/transparent_hugepage/shmem_enabled advise
-write /sys/kernel/mm/transparent_hugepage/khugepaged/pages_to_scan 2048
-write /sys/kernel/mm/transparent_hugepage/khugepaged/scan_sleep_millisecs 5000
-write /sys/kernel/mm/transparent_hugepage/khugepaged/alloc_sleep_millisecs 50000
 write /sys/kernel/mm/transparent_hugepage/khugepaged/max_ptes_swap 128
 
 # flash
@@ -86,22 +80,22 @@ write /sys/block/zram*/queue/add_random 0
 write /sys/block/mmcblk*/queue/add_random 0
 write /sys/block/nvme*n*/queue/add_random 0
 # zram
-write /sys/block/zram*/queue/read_ahead_kb 512
+write /sys/block/zram*/queue/read_ahead_kb 0
 # microSD
-write /sys/block/mmcblk*/queue/read_ahead_kb 2048
+write /sys/block/mmcblk*/queue/read_ahead_kb 512
 write /sys/block/mmcblk*/queue/rq_affinity 2
-write /sys/block/mmcblk*/queue/wbt_lat_usec 2000
-write /sys/block/mmcblk*/queue/iosched/back_seek_penalty 1
-write /sys/block/mmcblk*/queue/iosched/fifo_expire_async 200
-write /sys/block/mmcblk*/queue/iosched/fifo_expire_sync 100
-write /sys/block/mmcblk*/queue/iosched/slice_idle 0
-write /sys/block/mmcblk*/queue/iosched/slice_idle_us 0
-write /sys/block/mmcblk*/queue/iosched/timeout_sync 100
+#write /sys/block/mmcblk*/queue/wbt_lat_usec 2000
+#write /sys/block/mmcblk*/queue/iosched/back_seek_penalty 1
+#write /sys/block/mmcblk*/queue/iosched/fifo_expire_async 200
+#write /sys/block/mmcblk*/queue/iosched/fifo_expire_sync 100
+#write /sys/block/mmcblk*/queue/iosched/slice_idle 0
+#write /sys/block/mmcblk*/queue/iosched/slice_idle_us 0
+#write /sys/block/mmcblk*/queue/iosched/timeout_sync 100
 # nvme
-write /sys/block/nvme*n*/queue/read_ahead_kb 1024
-write /sys/block/nvme*n*/queue/wbt_lat_usec 999
-write /sys/block/nvme*n*/queue/iosched/write_lat_nsec 6000000
-write /sys/block/nvme*n*/queue/iosched/read_lat_nsec 1200000
+write /sys/block/nvme*n*/queue/read_ahead_kb 256
+write /sys/block/nvme*n*/queue/rq_affinity 2
+write /sys/block/nvme*n*/queue/nomerges 2
+
 
 # BORE-scheduler
 #write /proc/sys/kernel/sched_bore 1
